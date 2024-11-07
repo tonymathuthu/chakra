97149362f4a7d6bd098c12695e0cf6a52899e885
tonymathuthu <official.tonymathuthu@gmail.com>
2024-11-07 19:40:17 +0200

    update

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("slidecontainer");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-  output.innerHTML = this.value;
-};
